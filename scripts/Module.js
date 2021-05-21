25062330deeaa370c69434d69d8e7af0c5df27ca
Yena-Yun <cheryleduart@gmail.com>
2021-05-21 14:17:01 +0900

    module

---
+++ scripts/Module.js
@@ -0,0 +1,10 @@
+export function log(message) {
+	console.log(message);
+}
+
+export function error(message) {
+	console.error(message);
+}
+
+//함수뿐만 아니라 변수도 export 가능
+export const PI = 3.14;
---
+++ scripts/Ref.js
@@ -0,0 +1,5 @@
+//다른 js 파일의 함수를 import 해와서
+import { log } from "/scripts/Module.js";
+
+//여기서 인자만 넣어주고 실행은 html의 script에서 할 수 있음
+log("Ref에서 출력하는 메시지");
